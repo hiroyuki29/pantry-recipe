85edd03ba491ac3f529398db53dd037e524d4b6a
hiroyuki29 <hiroyuki0209@gmail.com>
2022-03-05 21:36:55 +0900

    add item controller

---
+++ app/controllers/v1/items_controller.rb
@@ -0,0 +1,46 @@
+module V1
+  class V1::ItemsController < ApplicationController
+    before_action :set_item, only: [:show, :update, :destroy]
+
+    def index
+        items = Item.order(id: :desc)
+        render json: { status: 'SUCCESS', message: 'Loaded items', data: items }
+    end
+
+    def show
+        render json: { status: 'SUCCESS', message: 'Loaded the item', data: @item }
+    end
+
+    def create
+        item = Item.new(item_params)
+        if item.save
+            render json: { status: 'SUCCESS', data: item }
+        else
+            render json: { status: 'ERROR', data: item.errors }
+        end
+    end
+
+    def destroy
+        @item.destroy
+        render json: { status: 'SUCCESS', message: 'Deleted the item', data: @item }
+    end
+
+    def update
+        if @item.update(item_params)
+            render json: { status: 'SUCCESS', message: 'Update the item', data: @item }
+        else
+            render json: { status: 'ERROR', message: 'Not updated', data: @item.errors }
+        end
+    end
+
+    private
+
+    def set_item
+        @item = Item.find(params[:id])
+    end
+
+    def item_params
+        params.requier(:item).permit(:name, :category_id, :master_food_id, :unit_quantity)
+  end
+
+end
