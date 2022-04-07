d2be58092d7c4d6b21b08cc60604a3536cb3768a
hiroyuki29 <hiroyuki0209@gmail.com>
2022-04-08 05:37:56 +0900

    modify logic to reduce DB wastefulness

--- app/controllers/v1/user_items_controller.rb
+++ app/controllers/v1/user_items_controller.rb
@@ -1,14 +1,29 @@
 module V1
   class V1::UserItemsController < ApplicationController
+    before_action :set_item, only: [:destroy]
 
     def index
-      items = current_v1_user.items.order(:category_id, :item_id)
+      items = current_v1_user.items.preload(:user_items).select('user_items.*, items.name, items.category_id, items.master_food_id, items.unit_quantity').order(:category_id, :item_id)
       render json: { status: 'SUCCESS', data: items }
     end
+
     def create
       item = Item.find(params[:item_id])
         current_v1_user.store_to_user(item)
         render json: { status: 'SUCCESS', data: item }
     end
+
+    def destroy
+      @item.destroy
+      render json: { status: 'SUCCESS', message: 'Deleted the item', data: @item }
+    end
+
+    private
+
+    def set_item
+        @item = UserItem.find(params[:id])
+    end
+
+
   end
 end
